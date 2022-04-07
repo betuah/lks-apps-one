15588336369575f73888dd29ca9a94ae09b18386
betuah <betuahanugerah@gmail.com>
2022-04-07 15:24:49 +0700

    remove husky

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -35,10 +35,6 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module',
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
   ],
